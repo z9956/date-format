3ae72807a505d316abab4d67141b5ecc88b2d34e
z9956 <zhz995617@gmail.com>
2021-07-02 18:57:23 +0800

    feat: valueOf、unix

--- src/main.js
+++ src/main.js
@@ -1,7 +1,66 @@
 class DateFormat {
-	constructor() {}
+	constructor(dateString) {
+		this.parse(dateString);
+	}
 
-	valueOf() {}
+	parse(dateString) {
+		this.$d = parseDate(dateString);
+		this.init();
+	}
+
+	init() {
+		const $d = this.$d;
+
+		this.$y = $d.getFullYear();
+		this.$M = $d.getMonth();
+		this.$D = $d.getDate();
+		this.$W = $d.getDay();
+		this.$H = $d.getHours();
+		this.$m = $d.getMinutes();
+		this.$s = $d.getSeconds();
+		this.$ms = $d.getMilliseconds();
+	}
+
+	valueOf() {
+		return this.$d.getTime();
+	}
+
+	unix() {
+		return Math.floor(this.valueOf() / 1000);
+	}
+
+	format(formatStr) {
+		return this.$d;
+	}
+
+	diff() {}
+
+	add() {}
+
+	set() {}
+
+	get() {}
+
+	year() {
+		return this.$y;
+	}
 }
 
-export default DateFormat;
+/**
+ * @param { string } dateString
+ */
+const parseDate = (dateString) => {
+	if (typeof dateString === 'string') {
+		return new Date(Date.parse(dateString));
+	}
+
+	return new Date();
+};
+
+/**
+ * @param { string } dateString
+ */
+const dateFormat = (dateString) => {
+	return new DateFormat(dateString);
+};
+export default dateFormat;
